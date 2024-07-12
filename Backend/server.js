b93cb55e52e4b7646dfd931ba155184378e79c23
swathimuneeswaran <swathirajan255@gmail.com>
2024-07-12 19:51:16 +0530

    commit

--- Backend/server.js
+++ Backend/server.js
@@ -1,8 +1,8 @@
-import express from 'express';
+import express from "express";
 import dotenv from "dotenv";
 import path from "path";
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { dirname } from "path";
 import userRoutes from "./routes/userRoutes.js";
 // import cors from "cors";
 import postRoutes from "./routes/postRoutes.js";
@@ -11,19 +11,15 @@ dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const allowedOrigins = ['http://localhost:5173'];
 
 app.use(express.json());
 
-
-
 app.use("/api/auth", userRoutes);
-app.use('/api/posts', postRoutes);
+app.use("/api/posts", postRoutes);
 
 app.use(express.static(path.join(__dirname, "../Frontend/dist")));
 app.get("*", (req, res) => {
